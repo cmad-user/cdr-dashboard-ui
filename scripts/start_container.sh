9ca2ab92a34f6caa1ab65dabf7f429121948a245
dedadhic <dadhich.dev@gmail.com>
2019-03-21 08:01:55 +0530

    pushing latest changes

--- scripts/start_container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-echo "Logging in to Amazon ECR..."
-$(aws ecr get-login --no-include-email --region us-west-2)
-echo "Running docker image"
-sudo docker run -d --name cdr-dashboard-ui -p 3000:3000 860360332628.dkr.ecr.us-west-2.amazonaws.com/cdr-dashboard-ui:latest
